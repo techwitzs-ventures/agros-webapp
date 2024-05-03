6472c4a46cbb1db0df06e595ec2248dacfbba449
Anand Vardhan <anand.vardhan@conqudel.com>
2024-05-03 15:56:46 +0530

    seller id hardcoded

--- src/app/main/apps/order/store/single_purchase_order_Slice.js
+++ src/app/main/apps/order/store/single_purchase_order_Slice.js
@@ -70,7 +70,7 @@ const singlepurchaseorderSlice = createSlice({
         payload: {
           /* when multiple plateform sellers or sellers are available then ,
           this vendor is field will become a empty string. */
-          vendor_id: 'c56e3e85-fee8-4f35-943a-0081255acbf3',
+          vendor_id: '719426a1-dccf-49db-af42-ae1b4f4cd7ca',
           delivery_address: '',
           exp_delivery_date: '',
           item_list: [{
