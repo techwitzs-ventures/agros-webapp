4f80d2ce6744c790ec2fa3e2601c6c2e6a8d9fe8
Anand Vardhan <anand.vardhan@conqudel.com>
2024-05-28 12:18:06 +0530

    color theme changed for web-app

--- src/app/configs/themesConfig.js
+++ src/app/configs/themesConfig.js
@@ -13,7 +13,153 @@ export const darkPaletteText = {
   disabled: 'rgb(156, 163, 175)',
 };
 
+export const agroWebsitePaletteText = {
+  primary: '#004b1c',
+  secondary: '#004b1c',
+  disabled: 'rgb(156, 163, 175)',
+}
+
 const themesConfig = {
+  agrotoolbar: {
+    palette: {
+      mode: 'light',
+      divider: '#e2e8f0',
+      text: lightPaletteText,
+      common: {
+        black: 'rgb(17, 24, 39)',
+        white: 'rgb(255, 255, 255)',
+      },
+      primary: {
+        light: '#64748b',
+        main: '#1e293b',
+        dark: '#0f172a',
+        contrastText: darkPaletteText.primary,
+      },
+      secondary: {
+        light: '#818cf8',
+        main: '#004b1c',// button and rest component color
+        dark: '#003b16',// button hover color
+        contrastText: darkPaletteText.primary,
+      },
+      background: {
+        paper: '#FFFFFF',
+        default: '#f1f5f9',
+      },
+      error: {
+        light: '#ffcdd2',
+        main: '#f44336',
+        dark: '#b71c1c',
+      },
+    },
+    status: {
+      danger: 'orange',
+    },
+  },
+  agromain: {
+    palette: {
+      mode: 'light',
+      divider: '#e2e8f0',
+      text: lightPaletteText,
+      common: {
+        black: 'rgb(17, 24, 39)',
+        white: 'rgb(255, 255, 255)',
+      },
+      primary: {
+        light: '#64748b',
+        main: '#004b1c',// main color
+        dark: '#0f172a',
+        contrastText: darkPaletteText.primary,
+      },
+      secondary: {
+        light: '#818cf8',
+        main: '#004b1c',// button and rest component color
+        dark: '#003b16',// button hover color
+        contrastText: darkPaletteText.primary,
+      },
+      background: {
+        paper: '#FFFFFF',
+        default: '#FFFFFF',
+      },
+      error: {
+        light: '#ffcdd2',
+        main: '#f44336',
+        dark: '#b71c1c',
+      },
+    },
+    status: {
+      danger: 'orange',
+    },
+  },
+  agronavbar: {
+    palette: {
+      mode: 'dark',
+      divider: 'rgba(241,245,249,.12)',
+      text: agroWebsitePaletteText,
+      common: {
+        black: 'rgb(17, 24, 39)',
+        white: 'rgb(255, 255, 255)',
+      },
+      primary: {
+        light: '#64748b',
+        main: '#334155',
+        dark: '#0f172a',
+        contrastText: agroWebsitePaletteText.primary,
+      },
+      secondary: {
+        light: '#818cf8',
+        main: '#004b1c',// button and rest component color
+        dark: '#003b16',// button hover color
+        contrastText: agroWebsitePaletteText.primary,
+      },
+      background: {
+        paper: '#1e293b',
+        default: '#111827',
+      },
+      error: {
+        light: '#ffcdd2',
+        main: '#f44336',
+        dark: '#b71c1c',
+      },
+      status: {
+        danger: 'orange',
+      },
+    },
+  },
+  agrofooter: {
+    palette: {
+      mode: 'dark',
+      divider: 'rgba(241,245,249,.12)',
+      text: darkPaletteText,
+      common: {
+        black: 'rgb(17, 24, 39)',
+        white: 'rgb(255, 255, 255)',
+      },
+      primary: {
+        light: '#64748b',
+        main: '#334155',
+        dark: '#0f172a',
+        contrastText: darkPaletteText.primary,
+      },
+      secondary: {
+        light: '#818cf8',
+        main: '#4f46e5',
+        dark: '#3730a3',
+        contrastText: darkPaletteText.primary,
+      },
+      background: {
+        paper: '#1e293b',
+        default: '#111827',
+      },
+      error: {
+        light: '#ffcdd2',
+        main: '#f44336',
+        dark: '#b71c1c',
+      },
+      status: {
+        danger: 'orange',
+      },
+    },
+  },
   default: {
     palette: {
       mode: 'light',
